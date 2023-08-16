d588bb48f933c4f48f4f265dcd910121d6cd897c
Viacheslav <novikova835@gmail.com>
2023-08-16 09:34:12 +0300

    test in php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+function php_deepclient_select() {
+    return _php_deepclient_select();
+}
+
+function php_deepclient_insert() {
+    return _php_deepclient_insert();
+}
+
+echo php_deepclient_select() . "\n";
+echo php_deepclient_insert() . "\n";
+?>
