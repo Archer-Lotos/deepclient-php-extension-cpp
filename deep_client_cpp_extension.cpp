ebd18b8dc64333a5ad7ff8ec54a9c905270c615a
Viacheslav <novikova835@gmail.com>
2023-08-17 08:39:39 +0300

    fix run  PHP library

--- deep_client_cpp_extension.cpp
+++ deep_client_cpp_extension.cpp
@@ -6,7 +6,7 @@ extern "C" {
 
 using namespace boost::python;
 
-PHP_FUNCTION(php_make_deep_client) {
+PHP_FUNCTION(make_deep_client) {
     char *token;
     char *url;
     size_t token_len, url_len;
@@ -54,62 +54,6 @@ PHP_FUNCTION(php_make_deep_client) {
     Py_Finalize();
 }
 
-PHP_FUNCTION(php_deepclient_select) {
-    try {
-        PyObject *pModule = PyImport_ImportModule("deep_client_interface");
-        if (pModule) {
-            PyObject *pClass = PyObject_GetAttrString(pModule, "DeepClient");
-            if (pClass && PyCallable_Check(pClass)) {
-                PyObject *pInstance = PyObject_CallObject(pClass, NULL);
-                PyObject *pMethod = PyObject_GetAttrString(pInstance, "select");
-                if (pMethod && PyCallable_Check(pMethod)) {
-                    PyObject *pValue = PyObject_CallObject(pMethod, NULL);
-                    if (pValue) {
-                        const char *result = PyUnicode_AsUTF8(pValue);
-                        zend_string *result_str = zend_string_init(result, strlen(result), 0);
-                        RETURN_STR(result_str);
-                    }
-                    Py_XDECREF(pValue);
-                }
-                Py_XDECREF(pMethod);
-                Py_XDECREF(pInstance);
-            }
-            Py_XDECREF(pClass);
-            Py_DECREF(pModule);
-        }
-    } catch (error_already_set const &) {
-        PyErr_Print();
-    }
-}
-
-PHP_FUNCTION(php_deepclient_insert) {
-    try {
-        PyObject *pModule = PyImport_ImportModule("deep_client_interface");
-        if (pModule) {
-            PyObject *pClass = PyObject_GetAttrString(pModule, "DeepClient");
-            if (pClass && PyCallable_Check(pClass)) {
-                PyObject *pInstance = PyObject_CallObject(pClass, NULL);
-                PyObject *pMethod = PyObject_GetAttrString(pInstance, "insert");
-                if (pMethod && PyCallable_Check(pMethod)) {
-                    PyObject *pValue = PyObject_CallObject(pMethod, NULL);
-                    if (pValue) {
-                        const char *result = PyUnicode_AsUTF8(pValue);
-                        zend_string *result_str = zend_string_init(result, strlen(result), 0);
-                        RETURN_STR(result_str);
-                    }
-                    Py_XDECREF(pValue);
-                }
-                Py_XDECREF(pMethod);
-                Py_XDECREF(pInstance);
-            }
-            Py_XDECREF(pClass);
-            Py_DECREF(pModule);
-        }
-    } catch (error_already_set const &) {
-        PyErr_Print();
-    }
-}
-
 ZEND_BEGIN_ARG_INFO(arginfo_make_deep_client, 0)
                 ZEND_ARG_INFO(0, token)
                 ZEND_ARG_INFO(0, url)
@@ -122,9 +66,7 @@ PHP_MINFO_FUNCTION(deep_client_php_extension) {
 }
 
 zend_function_entry deep_client_php_extension_functions[] = {
-        PHP_FE(php_make_deep_client, arginfo_make_deep_client)
-        PHP_FE(php_deepclient_select, NULL)
-        PHP_FE(php_deepclient_insert, NULL)
+        PHP_FE(make_deep_client, arginfo_make_deep_client)
         PHP_FE_END
 };
 
@@ -141,6 +83,4 @@ zend_module_entry deep_client_php_extension_module_entry = {
         STANDARD_MODULE_PROPERTIES
 };
 
-#ifdef COMPILE_DL_DEEP_CLIENT_CPP_EXTENSION
 ZEND_GET_MODULE(deep_client_php_extension)
-#endif
--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
 
-function php_deepclient_select() {
-    return php_make_deep_client(
-    '',
-    ''
+function php_deep_client_select() {
+    return make_deep_client(
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsiYWRtaW4iXSwieC1oYXN1cmEtZGVmYXVsdC1yb2xlIjoiYWRtaW4iLCJ4LWhhc3VyYS11c2VyLWlkIjoiMzgwIn0sImlhdCI6MTY5MTkxMTQxM30.W0GOuqOvRZrgrVZkLaceKTPBitXwR-1WlxLgxUZXOnY',
+    'http://localhost:3006/gql'
     );
 }
 
-echo php_deepclient_select() . "\n";
+var_dump(php_deep_client_select());
 
 ?>
